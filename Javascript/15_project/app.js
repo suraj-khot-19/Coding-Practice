4dfd2b18eef94e8a002e9e946103f30c26553433
suraj-khot-19 <surajkhot19082001@gmail.com>
2024-08-26 15:48:20 +0530

    corrected api call

--- Javascript/15_project/app.js
+++ Javascript/15_project/app.js
@@ -1,183 +1,59 @@
 const countryList = {
-    AED: "AE",
-    AFN: "AF",
-    XCD: "AG",
-    ALL: "AL",
-    AMD: "AM",
-    ANG: "AN",
-    AOA: "AO",
-    AQD: "AQ",
-    ARS: "AR",
-    AUD: "AU",
-    AZN: "AZ",
-    BAM: "BA",
-    BBD: "BB",
-    BDT: "BD",
-    XOF: "BE",
-    BGN: "BG",
-    BHD: "BH",
-    BIF: "BI",
-    BMD: "BM",
-    BND: "BN",
-    BOB: "BO",
-    BRL: "BR",
-    BSD: "BS",
-    NOK: "BV",
-    BWP: "BW",
-    BYR: "BY",
-    BZD: "BZ",
-    CAD: "CA",
-    CDF: "CD",
-    XAF: "CF",
-    CHF: "CH",
-    CLP: "CL",
-    CNY: "CN",
-    COP: "CO",
-    CRC: "CR",
-    CUP: "CU",
-    CVE: "CV",
-    CYP: "CY",
-    CZK: "CZ",
-    DJF: "DJ",
-    DKK: "DK",
-    DOP: "DO",
-    DZD: "DZ",
-    ECS: "EC",
-    EEK: "EE",
-    EGP: "EG",
-    ETB: "ET",
     EUR: "FR",
-    FJD: "FJ",
-    FKP: "FK",
+    USD: "US",
+    JPY: "JP",
+    BGN: "BG",
+    CZK: "CZ",
+    DKK: "DK",
     GBP: "GB",
-    GEL: "GE",
-    GGP: "GG",
-    GHS: "GH",
-    GIP: "GI",
-    GMD: "GM",
-    GNF: "GN",
-    GTQ: "GT",
-    GYD: "GY",
-    HKD: "HK",
-    HNL: "HN",
-    HRK: "HR",
-    HTG: "HT",
     HUF: "HU",
+    PLN: "PL",
+    RON: "RO",
+    SEK: "SE",
+    CHF: "CH",
+    ISK: "IS",
+    NOK: "BV",
+    HRK: "HR",
+    RUB: "RU",
+    TRY: "TR",
+    AUD: "AU",
+    BRL: "BR",
+    CAD: "CA",
+    CNY: "CN",
+    HKD: "HK",
     IDR: "ID",
     ILS: "IL",
     INR: "IN",
-    IQD: "IQ",
-    IRR: "IR",
-    ISK: "IS",
-    JMD: "JM",
-    JOD: "JO",
-    JPY: "JP",
-    KES: "KE",
-    KGS: "KG",
-    KHR: "KH",
-    KMF: "KM",
-    KPW: "KP",
     KRW: "KR",
-    KWD: "KW",
-    KYD: "KY",
-    KZT: "KZ",
-    LAK: "LA",
-    LBP: "LB",
-    LKR: "LK",
-    LRD: "LR",
-    LSL: "LS",
-    LTL: "LT",
-    LVL: "LV",
-    LYD: "LY",
-    MAD: "MA",
-    MDL: "MD",
-    MGA: "MG",
-    MKD: "MK",
-    MMK: "MM",
-    MNT: "MN",
-    MOP: "MO",
-    MRO: "MR",
-    MTL: "MT",
-    MUR: "MU",
-    MVR: "MV",
-    MWK: "MW",
     MXN: "MX",
     MYR: "MY",
-    MZN: "MZ",
-    NAD: "NA",
-    XPF: "NC",
-    NGN: "NG",
-    NIO: "NI",
-    NPR: "NP",
     NZD: "NZ",
-    OMR: "OM",
-    PAB: "PA",
-    PEN: "PE",
-    PGK: "PG",
     PHP: "PH",
-    PKR: "PK",
-    PLN: "PL",
-    PYG: "PY",
-    QAR: "QA",
-    RON: "RO",
-    RSD: "RS",
-    RUB: "RU",
-    RWF: "RW",
-    SAR: "SA",
-    SBD: "SB",
-    SCR: "SC",
-    SDG: "SD",
-    SEK: "SE",
     SGD: "SG",
-    SKK: "SK",
-    SLL: "SL",
-    SOS: "SO",
-    SRD: "SR",
-    STD: "ST",
-    SVC: "SV",
-    SYP: "SY",
-    SZL: "SZ",
     THB: "TH",
-    TJS: "TJ",
-    TMT: "TM",
-    TND: "TN",
-    TOP: "TO",
-    TRY: "TR",
-    TTD: "TT",
-    TWD: "TW",
-    TZS: "TZ",
-    UAH: "UA",
-    UGX: "UG",
-    USD: "US",
-    UYU: "UY",
-    UZS: "UZ",
-    VEF: "VE",
-    VND: "VN",
-    VUV: "VU",
-    YER: "YE",
-    ZAR: "ZA",
-    ZMK: "ZM",
-    ZWD: "ZW",
+    ZAR: "ZA"
 };
 const url =
-    "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies";
+    "https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_hOPUvxLkKPnjzMHBnJkmT53sS8eKJruIpApLUWUH"
+
 
 let dropdowns = document.querySelectorAll('.dropdown select');
 const btn = document.querySelector('form button');
 const fromCurr = document.querySelector(".from select");
 const toCurr = document.querySelector(".to select");
+const msg = document.querySelector('.message');
 
 for (let selects of dropdowns) {
     for (country in countryList) {
         let newOption = document.createElement("option");
         newOption.innerText = country;
         newOption.value = country;
-        // from default selected USD
-        if (selects.name === "from" && country === 'USD') {
+        //    to default selected INR
+        if (selects.name === "to" && country === 'USD') {
             newOption.selected = 'selected';
         }
-        // to default selected INR
-        if (selects.name === "to" && country === 'INR') {
+        // from default selected USD
+        if (selects.name === "from" && country === 'INR') {
             newOption.selected = 'selected';
         }
         selects.append(newOption);
@@ -210,7 +86,28 @@ btn.addEventListener('click', (event) => {
         amount.value = "1";
     }
     // now modify url
-    let Url = `${url}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
-    let response = fetch(Url).then((res) => { console.log(res) });
-    console.log(response);
+    let Url = `${url}&base_currency=${fromCurr.value}&currencies=${toCurr.value}`;
+    // function call
+    fetching(Url, fromCurr.value, toCurr.value, amountVal);
 });
+// funcntion to cal and call
+async function fetching(URL, fromCurrency, toCurrency, amt) {
+    let response = await fetch(URL);
+    let jsonData = await response.json();
+    let data = jsonData.data;
+    myCurrRate = data[toCurrency.toString()];
+    let totalAmt = amt * myCurrRate;
+    msg.innerText = `${amt} ${fromCurrency} = ${totalAmt} ${toCurrency}`
+}
+
+
+/*
+demo url==> "https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_hOPUvxLkKPnjzMHBnJkmT53sS8eKJruIpApLUWUH&base_currency=INR&currencies=USD"
+
+output
+{
+    "data": {
+        "INR": 83.7408666468
+    }
+}
+*/
