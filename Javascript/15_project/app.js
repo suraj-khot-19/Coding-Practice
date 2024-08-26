02fcf1f0571b8cd55bb00394c5d9e09d1cbb36d8
suraj-khot-19 <surajkhot19082001@gmail.com>
2024-08-26 17:02:19 +0530

    added revese converter and loading effect

--- Javascript/15_project/app.js
+++ Javascript/15_project/app.js
@@ -12,7 +12,7 @@ const countryList = {
     SEK: "SE",
     CHF: "CH",
     ISK: "IS",
-    NOK: "BV",
+    NOK: "KR",
     HRK: "HR",
     RUB: "RU",
     TRY: "TR",
@@ -39,9 +39,10 @@ const url =
 
 let dropdowns = document.querySelectorAll('.dropdown select');
 const btn = document.querySelector('form button');
-const fromCurr = document.querySelector(".from select");
-const toCurr = document.querySelector(".to select");
+let fromCurr = document.querySelector(".from select");
+let toCurr = document.querySelector(".to select");
 const msg = document.querySelector('.message');
+const reverseIcon = document.querySelector('i');
 
 for (let selects of dropdowns) {
     for (country in countryList) {
@@ -71,6 +72,19 @@ const changeFlag = (change) => {
     const img = change.parentElement.querySelector('img');
     img.src = `https://flagsapi.com/${countryCode}/flat/64.png`;
 }
+const changeFlagManually = (from, to) => {
+    const fromCountryCode = countryList[from];
+    const toCountryCode = countryList[to];
+    //swap the selected
+    let temp = fromCurr.value;
+    fromCurr.value = toCurr.value;
+    toCurr.value = temp;
+    // accessing image through change
+    const img1 = document.querySelector('#img1');
+    const img2 = document.querySelector('#img2');
+    img1.src = `https://flagsapi.com/${toCountryCode}/flat/64.png`;
+    img2.src = `https://flagsapi.com/${fromCountryCode}/flat/64.png`;
+}
 
 // change on click btn
 btn.addEventListener('click', (event) => {
@@ -92,14 +106,40 @@ btn.addEventListener('click', (event) => {
 });
 // funcntion to cal and call
 async function fetching(URL, fromCurrency, toCurrency, amt) {
+    msg.innerHTML = "<img src='load.gif' alt='Loading...' height='20px'/>"
+    btn.innerHTML = "<img src='load.gif' alt='Loading...' height='20px'/>"
+    btn.style.backgroundColor = "white";
     let response = await fetch(URL);
     let jsonData = await response.json();
     let data = jsonData.data;
     myCurrRate = data[toCurrency.toString()];
     let totalAmt = amt * myCurrRate;
-    msg.innerText = `${amt} ${fromCurrency} = ${totalAmt} ${toCurrency}`
+    btn.innerHTML = "<button>Convert</button>"
+    btn.style.backgroundColor = "#663399";
+    msg.innerText = `${amt} ${fromCurrency} = ${totalAmt.toFixed(3)} ${toCurrency}`
 }
 
+// reverse the converter
+reverseIcon.addEventListener("click", (event) => {
+    // donot refresh page on change
+    event.preventDefault();
+    // changing flags
+    changeFlagManually(fromCurr.value, toCurr.value);
+
+    // access amt
+    let amount = document.querySelector('.amount input');
+    let amountVal = amount.value;
+    // handle negative
+    if (amountVal <= 0 || amountVal === "") {
+        // also display 1 if condition is true
+        amountVal = 1;
+        amount.value = "1";
+    }
+    // now modify url
+    let Url = `${url}&base_currency=${toCurr.value}&currencies=${fromCurr.value}`;
+    // function call
+    fetching(Url, toCurr.value, fromCurr.value, amountVal);
+});
 
 /*
 demo url==> "https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_hOPUvxLkKPnjzMHBnJkmT53sS8eKJruIpApLUWUH&base_currency=INR&currencies=USD"
